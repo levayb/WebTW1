b955a4501c16ab2a5a69ec94e065ab519338e62f
samufromhungary <blzkproblem@gmail.com>
2019-04-11 14:26:18 +0200

    init

--- src/main/java/com/codecool/web/servlet/LoginServlet.java
+++ src/main/java/com/codecool/web/servlet/LoginServlet.java
@@ -10,25 +10,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
-@WebServlet("/account")
-public class AccountServlet extends HttpServlet {
-
+@WebServlet("/logincheck")
+public class LoginServlet extends HttpServlet {
     private final AccountService service = new AccountService();
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        req.getRequestDispatcher("register.html").forward(req, resp);
-    }
-
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-        String username = req.getParameter("username");
-        String password = req.getParameter("pasword");
-        boolean isMentor = Boolean.valueOf(req.getParameter("selection"));
-        String email = req.getParameter("email");
-        service.addAccount(new Account(username, password, isMentor, email));
         resp.sendRedirect("index.html");
     }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        for (Account acc: service.getAccountList()) {
+            if(acc.getUsername().equalsIgnoreCase(username)){
+                if(acc.getPassword().equalsIgnoreCase(password)){
+                    resp.sendRedirect("Home.jsp");
+                }
+            }else{
+                resp.sendRedirect("index.html");
+            }
+
+        }
+    }
 }
